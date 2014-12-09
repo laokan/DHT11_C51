6e6903a4e864f18bcff879d80f32f6b39204acad
laokan <laokanlinux@gmail.com>
2014-12-09 21:54:23 +0800

    first run version

--- dht11.c
+++ dht11.c
@@ -6,71 +6,71 @@ unsigned char values[5];
 
 void DHT11_init()
 {
-   delay_ms(1000);
+	delay_ms(1000);
 }
 
 unsigned char get_byte()
 {
-   unsigned char s = 0;
-   unsigned char value = 0;
-	
-   for(s = 0; s < 8; s += 1)
-   {
-      value <<= 1;
-      while(!DHT11_pin);
-      Delay30us();
+	unsigned char s = 0;
+	unsigned char value = 0;
 
-      if(DHT11_pin)
-      {
-          value |= 1; 
-      } 
-      while(DHT11_pin);
-   } 
-   return value; 
+	for(s = 0; s < 8; s += 1)
+	{
+		value <<= 1;
+		while(!DHT11_pin);
+		Delay30us();
+
+		if(DHT11_pin)
+		{
+			value |= 1; 
+		} 
+		while(DHT11_pin);
+	} 
+	return value; 
 } 
 unsigned char get_data()
 {
-   short chk = 0;
-   unsigned char s = 0;
-   unsigned char check_sum = 0;
+	short chk = 0;
+	unsigned char s = 0;
+	unsigned char check_sum = 0;
 
-   DHT11_pin = 1;
-   DHT11_pin = 0;
-   delay_ms(18);
-   DHT11_pin = 1;
-   Delay26us();
+	DHT11_pin = 1;
+	DHT11_pin = 0;
+	delay_ms(18);
+	DHT11_pin = 1;
+	Delay26us();
 
-   chk = DHT11_pin;
-   if(chk)
-   {
-      return 1;
-   }
-   
-   Delay80us();
-   
-   chk = DHT11_pin;
-   if(!chk)
-   {
-      return 2;
-   }
-   Delay80us();
-   
-   for(s = 0; s <= 4; s += 1)
-   {
-       values[s] = get_byte();
-   }
+	chk = DHT11_pin;
+	if(chk)
+	{
+		return 1;
+	}
 
-   DHT11_pin=1;
+	Delay80us();
 
-   for(s = 0; s < 4; s += 1)
-   {
-       check_sum += values[s];
-   }
+	chk = DHT11_pin;
+	if(!chk)
+	{
+		return 2;
+	}
+	Delay80us();
 
-   if(check_sum != values[4])
-   {
-      return 3;
-   }else{
-      return 0;
-   }
+	for(s = 0; s <= 4; s += 1)
+	{
+		values[s] = get_byte();
+	}
+
+	DHT11_pin=1;
+
+	for(s = 0; s < 4; s += 1)
+	{
+		check_sum += values[s];
+	}
+
+	if(check_sum != values[4])
+	{
+		return 3;
+	}else{
+		return 0;
+	}
 }
--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 #include "uart.h"
 #include "delay.h"
 #include "dht11.h"
+#include <stdio.h>
 
 extern unsigned char values[5];
+char tmpstr[6];
+void int2str(int i, int j, char *s);
 void setup();
 
 void main()
@@ -32,8 +35,15 @@ void main()
 			}
 			default:
 			{
-				uartSendString("Tmp/ C: ");
-				uartSendString("R.H/ %:");
+				uartSendString("T:");
+				int2str((int)values[2], (int)values[3], tmpstr);
+				uartSendString(tmpstr);
+				uartSendString("\r\n");
+				
+				uartSendString("R.H%:");
+				int2str((int)values[0], (int)values[1], tmpstr);
+				uartSendString(tmpstr);
+				uartSendString("\r\n");
 				//printf(lcd_putc, "%3u.%03u ", values[2], values[3]);
 				//printf(lcd_putc, "%3u.%03u ", values[0], values[1]);
 				break;
@@ -46,4 +56,8 @@ void setup()
 {
 	DHT11_init();
 	uartInit();
-} 
+}
+void int2str(int i, int j, char *s)
+{
+	sprintf(s,"%3u.%03u",i,j);
+}
